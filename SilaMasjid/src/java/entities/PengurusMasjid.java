09d674589a8daf9d4fbe14e119b042521d63a2c4
unluckiers@gmail.com <unluckiers@gmail.com@f692df68-8ade-2df9-1078-8fb923b76ce4>
2011-11-09 14:37:39 +0000

    membuat package baru : entities, jpa membuat entitas baru : PengurusMasjid

---
+++ SilaMasjid/src/java/entities/PengurusMasjid.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class PengurusMasjid implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nama;
+    private String jabatan;
+    private String notelp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getJabatan() {
+        return jabatan;
+    }
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNotelp() {
+        return notelp;
+    }
+
+    public void setNotelp(String notelp) {
+        this.notelp = notelp;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PengurusMasjid)) {
+            return false;
+        }
+        PengurusMasjid other = (PengurusMasjid) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.PengurusMasjid[ id=" + id + " ]";
+    }
+    
+}
