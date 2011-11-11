2b48b05325661611cf4e68c5f6b81df2f8226784
unluckiers@gmail.com <unluckiers@gmail.com@f692df68-8ade-2df9-1078-8fb923b76ce4>
2011-11-11 18:30:33 +0000

    membuat entities : masjid, daftar masjid membuat servlet : register dan processregister
    catatan : masih ada error

---
+++ SilaMasjid/src/java/entities/DaftarMasjid.java
@@ -0,0 +1,139 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import jpa.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class DaftarMasjid implements Serializable {
+
+    public DaftarMasjid() {
+        emf = Persistence.createEntityManagerFactory("masjid");
+    }
+    @Id
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public boolean check(String email, String password) {
+        boolean result = false;
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT count(o) FROM Masjid AS o WHERE o.email=:email AND o.password=:password");
+            q.setParameter("email", email);
+            q.setParameter("password", password);
+            int jumlahMasjid = ((Long) q.getSingleResult()).intValue();
+            if (jumlahMasjid > 0) {
+                result = true;
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+
+    public Masjid getMasjid(String email, String password) {
+        Masjid masjid = null;
+        EntityManager em = getEntityManager();
+        try {
+            boolean hasilCheck = this.check(email, password);
+            if (hasilCheck) {
+                Query q = em.createQuery("SELECT object(o) FROM Masjid AS o WHERE o.email=:email AND o.password=:password");
+                q.setParameter("email", email);
+                q.setParameter("password", password);
+                masjid = (Masjid) q.getSingleResult();
+            }
+        } finally {
+            em.close();
+        }
+        return masjid;
+    }
+
+    public List<Masjid> getMasjid() {
+        List<Masjid> masjids = new ArrayList<Masjid>();
+
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT object(o) FROM Masjid AS o");
+            masjids = q.getResultList();
+
+        } finally {
+            em.close();
+        }
+        return masjids;
+    }
+
+    public void editMasjid(Masjid masjid) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.merge(masjid);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void addMasjid(Masjid masjid) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(masjid);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteMasjid(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Masjid masjid;
+            try {
+                masjid = em.getReference(Masjid.class, id);
+                masjid.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The user with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(masjid);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+}
---
+++ SilaMasjid/src/java/entities/Masjid.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class Masjid implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String password;
+    private String nmMasjid;
+    private String almtMasjid;
+    private String kotaMasjid;
+    private String telpMasjid;
+
+    public String getAlmtMasjid() {
+        return almtMasjid;
+    }
+
+    public void setAlmtMasjid(String almtMasjid) {
+        this.almtMasjid = almtMasjid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKotaMasjid() {
+        return kotaMasjid;
+    }
+
+    public void setKotaMasjid(String kotaMasjid) {
+        this.kotaMasjid = kotaMasjid;
+    }
+
+    public String getNmMasjid() {
+        return nmMasjid;
+    }
+
+    public void setNmMasjid(String nmMasjid) {
+        this.nmMasjid = nmMasjid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelpMasjid() {
+        return telpMasjid;
+    }
+
+    public void setTelpMasjid(String telpMasjid) {
+        this.telpMasjid = telpMasjid;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Masjid)) {
+            return false;
+        }
+        Masjid other = (Masjid) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Masjid[ id=" + id + " ]";
+    }
+    
+}
