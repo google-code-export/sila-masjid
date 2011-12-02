6f8359ed8dca5477240979377b7911a2027b37c2
unluckiers@gmail.com <unluckiers@gmail.com@f692df68-8ade-2df9-1078-8fb923b76ce4>
2011-12-02 17:32:40 +0000

    Issue 19:	coding - entities : proposal: -membuat entitas Proposal dan DaftarProposal -membuat servlet UnggahProposalServlet dan ProsesUnggahProposalServlet -membuat file jsp proposal.jsp

---
+++ SilaMasjid/src/java/entities/DaftarProposal.java
@@ -0,0 +1,152 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * @author Sumurmunding
+ */
+package entities;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import jpa.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class DaftarProposal implements Serializable {
+
+    public DaftarProposal() {
+        emf = Persistence.createEntityManagerFactory("SilaMasjidPU");
+    }
+    @Id
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public boolean checkId(Long id) {
+        boolean result = false;
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT count(o) FROM Proposal AS o WHERE o.id=:id");
+            q.setParameter("id", id);
+            int jumlahMasjid = ((Long) q.getSingleResult()).intValue();
+            if (jumlahMasjid > 0) {
+                result = true;
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+
+    public boolean check(String email) {
+        boolean result = false;
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT count(o) FROM Proposal AS o WHERE o.email=:email");
+            q.setParameter("email", email);
+            int jumlahMasjid = ((Long) q.getSingleResult()).intValue();
+            if (jumlahMasjid > 0) {
+                result = true;
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+    public Masjid getMasjid(Long id) {
+        Masjid masjid = null;
+        EntityManager em = getEntityManager();
+        try {
+            boolean hasilCheck = this.checkId(id);
+            if (hasilCheck) {
+                Query q = em.createQuery("SELECT object(o) FROM Proposal AS o WHERE o.id=:id");
+                q.setParameter("id", id);
+                masjid = (Masjid) q.getSingleResult();
+            }
+        } finally {
+            em.close();
+        }
+        return masjid;
+    }
+
+    public List<Proposal> getProposals(Long id) {//
+        List<Proposal> proposals = new ArrayList<Proposal>();
+
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT object(o) FROM Proposal AS o where o.id=:id");
+            proposals = q.getResultList();
+
+        } finally {
+            em.close();
+        }
+        return proposals;
+    }
+
+    public void editProposal(Proposal proposal) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.merge(proposal);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void addProposal(Proposal proposal) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(proposal);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteProposal(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Proposal proposal;
+            try {
+                proposal = em.getReference(Proposal.class, id);
+                proposal.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The user with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(proposal);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+}
---
+++ SilaMasjid/src/java/entities/Proposal.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class Proposal implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nama;
+    private String alamat;
+    private String noTelp;
+    private String email;
+    private Integer besarDana;
+    private String judul;
+    private String detailProposal;
+    private boolean setuju = false;
+    private Long idMasjid;
+
+    public Long getIdMasjid() {
+        return idMasjid;
+    }
+
+    public void setIdMasjid(Long idMasjid) {
+        this.idMasjid = idMasjid;
+    }
+
+    public boolean isSetuju() {
+        return setuju;
+    }
+
+    public void setSetuju(boolean setuju) {
+        this.setuju = setuju;
+    }
+    
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public Integer getBesarDana() {
+        return besarDana;
+    }
+
+    public void setBesarDana(Integer besarDana) {
+        this.besarDana = besarDana;
+    }
+
+    public String getDetailProposal() {
+        return detailProposal;
+    }
+
+    public void setDetailProposal(String detailProposal) {
+        this.detailProposal = detailProposal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Proposal)) {
+            return false;
+        }
+        Proposal other = (Proposal) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Proposal[ id=" + id + " ]";
+    }
+    
+}
