16cc3075a3e4536b609a591d7e53ceab46852e86
ecxo.doc@gmail.com <ecxo.doc@gmail.com@f692df68-8ade-2df9-1078-8fb923b76ce4>
2011-12-05 16:30:27 +0000

    membuat berita dan index berita
    UC : Berita
    Issue 21

---
+++ trunk/SilaMasjid/src/java/entities/IndexBerita.java
@@ -0,0 +1,153 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * @author Sumurmunding
+ */
+package entities;
+
+import entities.Berita;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Id;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import jpa.exceptions.NonexistentEntityException;
+
+/**
+ *
+ * @author Sumurmunding
+ */
+@Entity
+public class IndexBerita implements Serializable {
+
+    public IndexBerita() {
+        emf = Persistence.createEntityManagerFactory("SilaMasjidPU");
+    }
+    @Id
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public boolean checkId(Long id) {
+        boolean result = false;
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT count(o) FROM Berita AS o WHERE o.id=:id");
+            q.setParameter("id", id);
+            int jumlahMasjid = ((Long) q.getSingleResult()).intValue();
+            if (jumlahMasjid > 0) {
+                result = true;
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+
+    public boolean check(String email) {
+        boolean result = false;
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT count(o) FROM Berita AS o WHERE o.email=:email");
+            q.setParameter("email", email);
+            int jumlahMasjid = ((Long) q.getSingleResult()).intValue();
+            if (jumlahMasjid > 0) {
+                result = true;
+            }
+        } finally {
+            em.close();
+        }
+        return result;
+    }
+    public Masjid getMasjid(Long id) {
+        Masjid masjid = null;
+        EntityManager em = getEntityManager();
+        try {
+            boolean hasilCheck = this.checkId(id);
+            if (hasilCheck) {
+                Query q = em.createQuery("SELECT object(o) FROM Berita AS o WHERE o.id=:id");
+                q.setParameter("id", id);
+                masjid = (Masjid) q.getSingleResult();
+            }
+        } finally {
+            em.close();
+        }
+        return masjid;
+    }
+
+    public List<Berita> getBeritas(Long id) {//
+        List<Berita> beritas = new ArrayList<Berita>();
+
+        EntityManager em = getEntityManager();
+        try {
+            Query q = em.createQuery("SELECT object(o) FROM Berita AS o where o.id=:id");
+            beritas = q.getResultList();
+
+        } finally {
+            em.close();
+        }
+        return beritas;
+    }
+
+    public void editBerita(Berita berita) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.merge(berita);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void addBerita(Berita berita) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(berita);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void deleteBerita(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Berita berita;
+            try {
+                berita = em.getReference(Berita.class, id);
+                berita.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The user with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(berita);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+}
---
+++ trunk/SilaMasjid/web/pages/posting.jsp
@@ -0,0 +1,49 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="entities.DaftarMasjid"%>
+<%@page import="entities.Masjid"%>
+<!DOCTYPE html>
+<%@include file='hometemplate.html' %>
+<html>
+    <tr> 
+<%--JUDUL DI SINI--%>
+        <td width="452" height="32" class="contentheader"><div align="center"><b>TULIS BERITA</b></div></td>
+    </tr>
+    <tr> 
+        <td><img src="images/main_content_header_under.gif" width="452" height="26"></td>
+    </tr>
+    <tr> 
+        <td class="mctop"><table width="94%" border="0" cellpadding="0" cellspacing="0">
+ <%--ISI MULAI DI SINI--%>
+                <form action="unggahberita" method="post" >
+
+                    <tr>
+                        <td width="10%">&nbsp;</td>
+                        <td>Judul</td><td><input type="text" name="judul" style="width: 160px"></td>
+                    </tr>
+                    <tr>
+                        <td width="10%">&nbsp;</td>
+                        <td>Isi Berita</td><td><textarea rows="10" cols="30"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td width="10%">&nbsp;</td>
+                        <td>&nbsp;</td><td><input type="submit" value="Unggah" >&nbsp;&nbsp;<input type="reset" value="Reset"></td>
+                    
+                    </tr>
+                    <tr><td colspan="3" align="center"><label align="center"><font color="red"><b>${error}</b></font></label></td></tr>
+                </form>
+ 
+<%--SISI SAMPAI DI SINI--%>
+            </table></td>
+    </tr>
+    <tr> 
+        <td class="mctop"><img src="images/mc_box_bottom.gif" alt="-" width="452" height="23"></td>
+    </tr>
+</table>
+<!-- COPYRIGHT - WWW.ZYMIC.COM - IF YOU REMOVE COPYRIGHT LEGAL ACTION WILL BE TAKEN -->
+<!-- WE TRACK COPYRIGHT ON A DAILY BASIS, WE KNOW WHO YOU ARE!                      -->
+<div align="center"><a href="http://www.zymic.com" target="_blank"><img src="images/copyright_DO_NOT_STEAL_ELSE_LEGAL_ACTION.gif" alt="Free Templates" width="422" height="15" border="0"></a></div></td>
+</tr>
+</table>
+</body>
+</html>
+
